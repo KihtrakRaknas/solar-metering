5fc571298aa89223cd00b2e70d9dcea24bcadef1
git-directory-deploy <>
2021-04-05 22:14:05 -0400

    added locally hosted api to access log file

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { MDBNavbar, MDBNavbarBrand, MDBNavItem, MDBNavLink } from 'mdbreact';
 
-export default function NavBar({isMaster}) {
+export default function NavBar({}) {
+    const [lastWrite, setLastWrite] = useState(null);
+
+    const checkLastWrite = ()=>{
+        fetch('http://localhost:4001/last-write').then(res=>res.json()).then(data=>{
+            console.log(`Last Write: ${data.lastWrite}`)
+            setLastWrite(data.lastWrite)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+    useEffect(()=>{
+        checkLastWrite()
+        setInterval(checkLastWrite, 1000*60*5) // Update last write every 5 minutes
+    }, [true])
+
+    const calcDiffInTime = (timestamp) =>{
+        const diff = (new Date()) - timestamp
+        const diffInHrs = diff/(1000*60*60)
+        return {hours: Math.floor(diffInHrs % 24), days: Math.floor(diffInHrs/24), recent: diffInHrs<1}
+    }
+
+    let timeObj = calcDiffInTime(lastWrite)
+    
     return (
         <>
             <nav>
@@ -12,7 +36,16 @@ export default function NavBar({isMaster}) {
                     <MDBNavLink to="/home">Graphs</MDBNavLink>
                     <MDBNavLink to="/full-table">Full Table</MDBNavLink>
                     <div className="ml-auto">
-                        {isMaster?"Master Device":isMaster==null?"Loading...":"View Only"}
+                        {lastWrite != null? 
+                            lastWrite == -1? 
+                                `No uploads have happened yet`
+                            :
+                                timeObj.recent?
+                                    `An upload recently took place`
+                                :
+                                    `Last upload was ${timeObj.hours} hours and ${timeObj.days} days ago`
+                        :
+                            "View Only"}
                     </div>
                 </MDBNavbar>
             </nav>
